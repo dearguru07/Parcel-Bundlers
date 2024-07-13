6e862069ffb1253a6a67234ffcd158a6ec71d8de
dearguru07 <pangalaguruyadav101@gmail.com>
2024-07-13 19:53:08 +0530

    'latest'

--- app.js
+++ app.js
@@ -1,45 +1,9 @@
-const parent = React.createElement("div", { id: "parent" }, [
-  (child = React.createElement(
-    "div",
-    { id: "child" },
-    (heading = React.createElement(
-      "h1",
-      { id: "heading" },
-      "Hello world....."
-    )),
-    (headingg = React.createElement(
-      "h2",
-      { id: "heading-2" },
-      "Hello world....."
-    ))
-  )),
-  (child = React.createElement(
-    "div",
-    { id: "child" },
-    (heading = React.createElement(
-      "h1",
-      { id: "heading" },
-      "Hello world....."
-    )),
-    (headingg = React.createElement(
-      "h2",
-      { id: "heading-2" },
-      "Hello world....."
-    ))
-  )),
-]);
+import React  from "react"
+import ReactDOM from "react-dom"
 
-// const newParent = React.createElement(
-//   "div",
-//   { id: "newParent" },
-//   React.createElement(
-//     "div",
-//     { id: "newChid" },
-//     React.createElement("h1", { id: "newHeading" }, "Happy Morng To all.."),
-//     React.createElement("h1", { id: "newHeading" }, "Hello every One......")
-//   )
-// );
-const conatiner = document.getElementById("box");
-const reactRoot = ReactDOM.createRoot(conatiner);
-reactRoot.render(parent);
-reactRoot.render(newParent);
+const parent=React.createElement("h1",{id:'parent'},"hello to all...")
+const conatiner=document.getElementById("box")
+
+const reactRoot=ReactDOM.createRoot(conatiner)
+reactRoot.render(parent)
+
